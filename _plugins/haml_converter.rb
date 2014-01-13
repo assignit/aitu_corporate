61cd46946a153950ced8ee9e68b146584b4d0e6f
braintapper <steven.ng@braintapper.com>
2014-01-13 11:55:24 -0500

    Set up for asset toolchain

--- _plugins/haml_converter.rb
+++ _plugins/haml_converter.rb
@@ -1,8 +1,19 @@
+# _plugins/haml_converter.rb
 module Jekyll
-  require 'haml'
+
   class HamlConverter < Converter
+
     safe true
     priority :low
+    
+    def setup 
+      return if @setup
+      require 'haml'
+      @setup = true
+    rescue
+      STDERR.puts 'do `gem install haml`'
+      raise FatalException.new("Missing dependency: haml")
+    end
 
     def matches(ext)
       ext =~ /haml/i
@@ -13,9 +24,11 @@ module Jekyll
     end
 
     def convert(content)    
+      setup
       engine = Haml::Engine.new(content)
       engine.render
     end
+
   end
 
 end
